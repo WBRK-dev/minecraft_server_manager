5755df939548685c603d8d8ecfbb2ef16f65bd01
WBR_K-dev <koertjrweberp@gmail.com>
2023-08-13 14:16:25 +0200

    Added config handler

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 import os
 import sys
+import json
 
 def errorMessage(message, command_list):
     print(message)
     if command_list: print(" list ( online )\n start [ server_name ]\n stop [ server_name ]\n attach [ server_name ]")
+
+def config(key, value): 
+    if os.path.isfile("config.json"):
+
+        f = open("config.json")
+        data = json.load(f)
+        # print(data["minecraft_servers_dir"])
+        if value != "":
+            print("No setter yet.")
+        else:
+            try:
+                return data[key]
+            except:
+                return False
+    else:
+        return False
+    
+mc_path = "/opt/minecraft/servers"
+if config("minecraft_servers_dir", ""):
+    mc_path = config("minecraft_servers_dir", "")
 
 if len(sys.argv) > 1:
     if sys.argv[1] == "list":
         if len(sys.argv) == 3 and sys.argv[2] == "online":
             os.system("screen -ls")
         else:
-            dirs = os.listdir("/opt/minecraft/servers")
+            dirs = os.listdir(mc_path)
 
             for dir in dirs:
-                if os.path.isdir("/opt/minecraft/servers/"+dir):
+                if os.path.isdir("{}/{}".format(mc_path, dir)):
                     print(dir)
     elif sys.argv[1] == "start":
-        if len(sys.argv) == 3 and os.path.isdir("/opt/minecraft/servers/"+sys.argv[2]):
+        if len(sys.argv) == 3 and os.path.isdir("{}/{}".format(mc_path, sys.argv[2])):
             print("Starting {}.".format(sys.argv[2]))
-            os.system("sh /opt/minecraft/servers/{}/start.sh".format(sys.argv[2]))
+            os.system("sh {}/{}/start.sh".format(mc_path, sys.argv[2]))
         else:
             errorMessage("ERROR: This server does not exist.", True)
     elif sys.argv[1] == "stop":
-        if len(sys.argv) == 3 and os.path.isdir("/opt/minecraft/servers/"+sys.argv[2]):
+        if len(sys.argv) == 3 and os.path.isdir("{}/{}".format(mc_path, sys.argv[2])):
             print("Stopping {}.".format(sys.argv[2]))
             os.system("screen -S {} -X stuff 'stop\n'".format(sys.argv[2]))
         else:
